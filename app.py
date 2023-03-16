5f218432df6a444fcd71b76aca576bc5a05be895
Louis <louis.dupand@gmail.com>
2023-03-17 00:51:59 +0100

    some changes but a problem with jena

--- app.py
+++ app.py
@@ -6,36 +6,38 @@ import streamlit as st  # pip install streamlit
 from owlready2 import *
 from requetes import dictionnaire
 
-#owlready2.JAVA_EXE = "C:\\path\\to\\java.exe"
 
 #---- Importation of the class and functions
-
 
 # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/ 
 st.set_page_config(page_title="Ontology Project", page_icon=":clapper:", layout="wide")
 
 # Chemin 
 
-path="ontolgy/ProjectV3.owl"
+path="ontolgy/ProjectV5.owl"
+#path="C:/Users/leodu/OneDrive/Documents/Projects/Projet_ontology/ontolgy/ProjectV3.owl"
 
 onto = get_ontology(path).load()
 
-#sync_reasoner_pellet(onto)
-sync_reasoner()
+st.write("Load effective")
+
+with onto:
+  sync_reasoner_pellet()
 
 world = onto.world
 
-st.write("load effective")
+st.write("Reasonner active")
+st.title(":movie_camera: Movies ontowlogy presentation")
+st.markdown("##")
 
 for key in dictionnaire.keys():
   if len(dictionnaire[key])>70:
     st.write("----")
     st.write(key)
     results = list(world.sparql(dictionnaire[key]))
-    res=[ str(result[0])[10:] for result in results]
+    res=[ str(result[0]) for result in results]
     for r in res:
       st.write(str(r))
-
 
 
 
--- requetes.py
+++ requetes.py
@@ -14,33 +14,84 @@ dictionnaire={"List_the_instances_of_the_class_Actor":
             
             "List_the_name_of_all_Thriller_movies":
             """
-            
+                        PREFIX owl: <http://www.w3.org/2002/07/owl#>
+                        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+                        PREFIX mv: <http://www.semanticweb.org/esilv/ontologies/2023/2/untitled-ontology-20#>
+
+                        SELECT ?d ?m
+                        WHERE {
+                                ?d mv:isDirectorOf ?m .
+                                ?m mv:genre ?genre .
+                                ?m rdf:type mv:Movie .
+                                FILTER (?genre = "Thriller")	
+                        }
             """,
             "List_the_name_of_all_Crime_Thriller_movies":
             """
+                        PREFIX owl: <http://www.w3.org/2002/07/owl#>
+                        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+                        PREFIX mv: <http://www.semanticweb.org/esilv/ontologies/2023/2/untitled-ontology-20#>
 
+                        SELECT DISTINCT ?t ?m
+                        WHERE {
+                                ?t mv:title ?title .
+                                ?m mv:genre ?genre .
+                                ?m rdf:type mv:Movie .
+                                FILTER (?genre = "Thriller" || ?genre = "Crime")	
+                        }
             """,
             "List_the_name_of_Actors_older_than_51_years":
             """
-                    PREFIX owl: <http://www.w3.org/2002/07/owl#>
-                    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-                    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-                    PREFIX mv: <http://www.semanticweb.org/esilv/ontologies/2023/2/untitled-ontology-20#>
+                        PREFIX owl: <http://www.w3.org/2002/07/owl#>
+                        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+                        PREFIX mv: <http://www.semanticweb.org/esilv/ontologies/2023/2/untitled-ontology-20#>
 
-                    SELECT ?actor
-                    WHERE {
-                        ?actor mv:age ?age .
-                        ?actor rdf:type mv:Actor .
-                        FILTER (?age >= 51)
-                    }
+                        SELECT ?actor
+                        WHERE {
+                                ?actor mv:age ?age .
+                                ?actor rdf:type mv:Actor .
+                                FILTER (?age >= 51)
+                        }
             """,
             "List_of_movies_that_are_played_in_theater_for_a_specific_day":
             """
-            
+
+
+                        PREFIX owl: <http://www.w3.org/2002/07/owl#>
+                        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+                        PREFIX mv: <http://www.semanticweb.org/esilv/ontologies/2023/2/untitled-ontology-20#>
+
+                        SELECT DISTINCT ?theatre ?m ?sd
+                        WHERE {
+                                ?nt mv:nameTheatre ?theatre .
+                                ?m mv:title ?Movie .
+                                ?dt mv:startDate ?sd
+                        }
             """,
             "A_query_that_contains_at_least_2_Optional_Graph_Patterns":
             """
-            
+
+                        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                        PREFIX owl: <http://www.w3.org/2002/07/owl#>
+                        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+                        PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+                        PREFIX mv: <http://www.semanticweb.org/esilv/ontologies/2023/2/untitled-ontology-20#>
+
+                        SELECT ?movie ?worker ?theatre
+                        WHERE {
+                                ?movie rdf:type mv:Movie .
+                                OPTIONAL {
+                                        ?movie mv:hasActor ?worker . 
+                                }
+                                OPTIONAL {
+                                        ?movie mv:hasTheatre ?theatre .
+                                        FILTER  NOT EXISTS { ?movie mv:hasActor ?worker .}
+                                }
+                        }
             """,
             "A_query_that_contains_at_least_2_alternatives_and_conjunctions":
             """
